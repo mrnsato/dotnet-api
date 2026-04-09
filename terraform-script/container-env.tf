ae74a65d336af7abd4208ca1dda82019db3240f2
mrnsato <mellissarochasantos@outlook.com>
2026-04-09 16:02:59 -0300

    Fixing some terraform errors

--- terraform-script/container-env.tf
+++ terraform-script/container-env.tf
@@ -1,32 +1,32 @@
-resource "azurerm_container_app_environment" "main" {
-  name                = var.container_env_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-
-  tags = var.tags
+# Container App Environment
+resource "azurerm_container_app_environment" "apimanager" {
+  name                = "apimanager-container-env"
+  location            = azurerm_resource_group.apimanager.location
+  resource_group_name = azurerm_resource_group.apimanager.name
 }
 
-resource "azurerm_container_app" "main" {
-  name                         = var.container_app_name
-  resource_group_name          = azurerm_resource_group.main.name
-  container_app_environment_id = azurerm_container_app_environment.main.id
+# Container App
+resource "azurerm_container_app" "apimanager" {
+  name                         = "api-manager-container"
+  resource_group_name          = azurerm_resource_group.apimanager.name
+  container_app_environment_id = azurerm_container_app_environment.apimanager.id
   revision_mode                = "Single"
 
   registry {
-    server               = azurerm_container_registry.main.login_server
-    username             = azurerm_container_registry.main.admin_username
+    server               = azurerm_container_registry.apimanager.login_server
+    username             = azurerm_container_registry.apimanager.admin_username
     password_secret_name = "acr-password"
   }
 
   secret {
     name  = "acr-password"
-    value = azurerm_container_registry.main.admin_password
+    value = azurerm_container_registry.apimanager.admin_password
   }
 
   template {
     container {
-      name   = var.container_app_name
-      image  = "${azurerm_container_registry.main.login_server}/${var.container_image}"
+      name   = "api-manager-container"
+      image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
       cpu    = 0.5
       memory = "1Gi"
     }
@@ -44,6 +44,15 @@ resource "azurerm_container_app" "main" {
       latest_revision = true
     }
   }
+}
 
-  tags = var.tags
+# Outputs
+output "container_app_url" {
+  value       = azurerm_container_app.apimanager.latest_revision_fqdn
+  description = "URL do Container App"
+}
+
+output "container_app_environment_id" {
+  value       = azurerm_container_app_environment.apimanager.id
+  description = "ID do Container App Environment"
 }
--- terraform-script/variables.tf
+++ terraform-script/variables.tf
@@ -1,5 +1,5 @@
 variable "docker_image_name" {
   type        = string
-  description = "Docker image name (e.g., myapp:latest or myregistry.azurecr.io/myapp:latest)"
-  default     = "myapp:latest"
+  description = "Docker image name"
+  default     = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
 }
